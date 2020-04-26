0405e01ebe55dfc26f4cc7b9841447972b616eee
Leshay Adams <leshayadams@Leshays-MBP.home>
2020-04-26 12:25:56 +0200

    add animation for search

--- src/Components/SearchForm.jsx
+++ src/Components/SearchForm.jsx
@@ -1,113 +1,110 @@
 import React, {useState} from 'react';
-import styled, { css, keyframes } from 'styled-components';
+import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import './Animations.css';
+
+
+const FormContainer = styled.div`
+  position: relative;
+`;
+
+const SearchContainer = styled.form`
+  height: 60px;
+`;
+
+const SearchInput = styled.input `
+  box-sizing: border-box;
+  padding: 0 40px 0 10px;
+  width: 100%;
+  height: 50px;
+  border: 4px solid #ffffff;
+  background: none;
+  color: #fff;
+  font-family: Roboto;
+  font-size: 16px;
+  font-weight: 400;
+  border-radius: 4px;
+
+  @media only screen and (min-width: 320px) {
+    width: 300px;
+  }
+
+  ::placeholder {
+    color: white;
+    opacity: 0.8;
+  }
+`;
+
+const SearchButton = styled.button`
+  background: none;
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 50px;
+  width: 50px;
+  padding: 0;
+  outline: 0;
+  border: 0;
+  cursor: pointer;
+`;
+
+const SearchIcon = styled(FontAwesomeIcon)`
+  width: 20px !important;
+  height: 20px;
+  font-family: Roboto;
+  color: white;
+`;
+
+const LargeSearchIcon = styled(FontAwesomeIcon)`
+  bottom: 50px;
+  color: white;
+  width: 60px !important;
+  height: 60px;
+`;
 
 const SearchForm = ({search, updateSearch, updateSearchValue}) => {
-  const [expanded, setExpanded] = useState(false);
+  const [onShrink, setOnShrink] = useState(false);
+  const [afterShrink, setAfterShrink] = useState(false);
 
-  const expand = () => {
-    setExpanded(true);
+  const onShrinkChange = () => {
+    setOnShrink(true);
+    afterShrinkChange();
   }
-  
-  const FormContainer = styled.div`
-    position: relative;
-  `;
 
-  const slideInFromLeft = keyframes`
-    0% {
-      transform: translateX(-100%);
-      opacity: 1;
+  const afterShrinkChange = () => {
+    setTimeout(() => {
+      setAfterShrink(true)
+    }, 400);
+  }
+
+  const searchContainerStyle = () => {
+    return {
+      display: afterShrink ? "block" : "none",
+      animationName: afterShrink && "slideInFromLeft",
+      animationDuration: afterShrink && "1s",
+      animationTimingFunction: afterShrink && "ease-out",
+      animationFillMode: afterShrink && "forwards",
     }
-    100% {
-      transform: translateX(0);
-      opacity: 1;
+   };
+
+   const largeSearchIconStyle = () => {
+    return {
+      display: afterShrink ? "none" : "block",
+      animationName: onShrink && "shrink",
+      animationDuration: onShrink && "1s", 
+      animationTimingFunction: onShrink && "ease-out",
+      animationFillMode: "forwards",
     }
-  `;
+   }
 
-  const shrink = keyframes`
-    from {
-      transform: scale(1);
-      opacity: 1;
-    }
-    to {
-      transform: scale(0);
-      opacity: 0;
-    }
-  `;
-
-  const SearchInput = styled.input `
-    box-sizing: border-box;
-    padding: 0 40px 0 10px;
-    width: 100%;
-    height: 50px;
-    border: 4px solid #ffffff;
-    background: none;
-    color: #fff;
-    font-family: Roboto;
-    font-size: 16px;
-    font-weight: 400;
-    border-radius: 4px;
-
-    @media only screen and (min-width: 320px) {
-      width: 300px;
-    }
-
-    ::placeholder {
-      color: white;
-      opacity: 0.8;
-    }
-  `;
-
-  const SearchButton = styled.button`
-    background: none;
-    position: absolute;
-    top: 0;
-    right: 0;
-    height: 50px;
-    width: 50px;
-    padding: 0;
-    outline: 0;
-    border: 0;
-    cursor: pointer;
-  `;
-
-  const SearchIcon = styled(FontAwesomeIcon)`
-    width: 20px !important;
-    height: 20px;
-    font-family: Roboto;
-    color: white;
-  `;
-
-  const LargeSearchIcon = styled(FontAwesomeIcon)`
-    position: relative;
-    bottom: 50px;
-    color: white;
-    width: 60px !important;
-    height: 60px;
-    animation-name: ${expanded ? css`${shrink}` : "none"};
-    animation-duration: ${expanded && "2s"}; 
-    animation-timing-function: ${expanded ? "ease-out" : "none"};
-    animation-fill-mode: forwards;
-    // animation: ${expanded && css`${shrink}`} ${expanded && "2s"}; ${expanded && "ease-out"} 0 0 0 forwards;
-  `;
-
-  const Search = styled.div`
-    opacity: 0;
-    animation-name: ${expanded ? css`${slideInFromLeft}` : "none"};
-    animation-duration: ${expanded ? "1s" : "none"}; 
-    animation-timing-function: ${expanded ? "ease-out" : "none"};
-    animation-delay: 1s;
-    animation-fill-mode: forwards;
-  `;
-
-  return ( 
-    <FormContainer onSubmit={updateSearchValue}>
-      <Search>
-        <SearchInput type="text" value={search} placeholder="eg. Salad" onChange={updateSearch} />
-        <SearchButton type="submit"><SearchIcon icon={faSearch} /></SearchButton>
-      </Search>
-      <LargeSearchIcon onClick={expand} icon={faSearch} />
+  return (
+    <FormContainer className="formContainer">
+      <SearchContainer style={searchContainerStyle()} onSubmit={updateSearchValue}>
+        <SearchInput type="text" value={search} placeholder="eg. Salad" onChange={updateSearch}/>
+        <SearchButton type="submit" ><SearchIcon icon={faSearch} /></SearchButton>
+      </SearchContainer>
+      <LargeSearchIcon style={largeSearchIconStyle()} icon={faSearch} onClick={onShrinkChange} />
     </FormContainer>
    );
 }
